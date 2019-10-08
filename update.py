eae5b4e2f0240567047317505f08e9a4a9cd179e
James Vogel <jim.m.vogel@gmail.com>
2019-10-07 23:48:08 -0500

    swithc to popen to read output

--- update.py
+++ update.py
@@ -2,7 +2,7 @@ import os
 
 
 os.system("git fetch")
-update_available = "behind" in os.system("git status")
+update_available = "behind" in os.popen("git status").read()
 
 if update_available:
     os.system("supervisorctl stop qboard")
